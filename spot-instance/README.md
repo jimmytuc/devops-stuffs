e034e2159a692eec81c6b02c722c19cc90bae20c
jimmytuc <jimmytucsolo@gmail.com>
2019-07-19 08:17:18 +0800

    spot-instance module

---
+++ spot-instance/README.md
@@ -0,0 +1,4 @@
+# devops-stuffs
+
+## spot-instance module
+
---
+++ spot-instance/data.tf
@@ -0,0 +1,11 @@
+data "aws_instances" "spot_fleet_request" {
+  instance_tags {
+    "aws:ec2spot:fleet-request-id" = "${aws_spot_fleet_request.spot_fleet_request.id}"
+  }
+
+  depends_on = ["aws_spot_fleet_request.spot_fleet_request"]
+}
+
+data "aws_instance" "spot_specific_instance" {
+  instance_id = "${data.aws_instances.spot_fleet_request.ids[length(data.aws_instances.spot_fleet_request.ids)]}"
+}
